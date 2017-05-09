ba081a45cca243508bd1bd7f9589bd74cf970806
andyferraro <andrea.ferraro@cnaf.infn.it>
2017-05-09 03:15:08 +0200

    Added OpenMP threads

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include <string>
 #include <chrono>
+#include <omp.h>
 
 using namespace std;
 
@@ -11,22 +12,29 @@ JNIEXPORT jdouble JNICALL
 Java_it_ttlab_piloopapp_MainActivity_piFromJNI(
         JNIEnv *env,
         jobject /* this */,
-        jlong num_steps) {
+        jlong num_steps,
+        jint num_threads) {
     double step;
     //long num_steps = 10000;
     int verbose = 0;
-    double x, pi, sum = 0.0;
-    double start_time, run_time;
-    chrono::high_resolution_clock::time_point t01, t02;
-    chrono::duration<double> dt1;
-    step = 1.0/(double) num_steps;
-    sum = 0.0;
-    t01 = chrono::high_resolution_clock::now();
-    for (int i=1;i<= num_steps; i++){
-        x = (i-0.5)*step;
-        sum = sum + 4.0/(1.0+x*x);
+    omp_set_num_threads(num_threads);
+        double pi, sum = 0.0;
+        double start_time, run_time;
+        chrono::high_resolution_clock::time_point t01, t02;
+        chrono::duration<double> dt1;
+        step = 1.0 / (double) num_steps;
+        sum = 0.0;
+        t01 = chrono::high_resolution_clock::now();
+#pragma omp parallel
+    {
+        double x;
+#pragma omp for reduction(+:sum)
+        for (int i = 1; i <= num_steps; i++) {
+            x = (i - 0.5) * step;
+            sum = sum + 4.0 / (1.0 + x * x);
+        }
     }
-    pi = step * sum;
+        pi = step * sum;
     t02 = chrono::high_resolution_clock::now();
     dt1 = chrono::duration_cast<chrono::duration<double>>(t02 - t01);
     mytime = dt1.count();
--- app/src/main/java/it/ttlab/piloopapp/MainActivity.java
+++ app/src/main/java/it/ttlab/piloopapp/MainActivity.java
@@ -1,12 +1,18 @@
 package it.ttlab.piloopapp;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import static it.ttlab.piloopapp.R.id.cppCheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,36 +22,93 @@ public class MainActivity extends AppCompatActivity {
     }
 
     Activity mActivity;
+    private EditText editText;
+    private TextView piTextView;
+    private static final String KEY_PI_TEXT = "key_pi_text";
+    //private TextView chronoTimeTextView;
+    private TextView cppTimeTextView;
+    private static final String KEY_CPP_TIME_TEXT = "key_cpp_time_text";
+    private TextView javaTimeTextView;
+    private static final String KEY_JAVA_TIME_TEXT = "key_java_time_text";
+    private CheckBox javaCheckBox;
+    private CheckBox cppCheckBox;
+    private int threadsNum = 1;
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        //setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putCharSequence(KEY_PI_TEXT,piTextView.getText());
+        outState.putCharSequence(KEY_CPP_TIME_TEXT,cppTimeTextView.getText());
+        outState.putCharSequence(KEY_JAVA_TIME_TEXT,javaTimeTextView.getText());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        //TextView tv = (TextView) findViewById(R.id.sample_text);
-        //tv.setText(stringFromJNI());
-        //tv.setText(String.valueOf(doubleFromJNI()));
+        Spinner spinner = (Spinner) findViewById(R.id.coresSpinner);
+        String[] threads = getResources().getStringArray(R.array.threads);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        threads);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        //                        android.R.layout.simple_spinner_item,
+        //                        threads);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv = (TextView) view;
+                threadsNum=Integer.parseInt(tv.getText().toString());
+                //m_tv.setText(tv.getText());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                threadsNum = 1 ;
+            }
+        });
+        if (savedInstanceState != null) {
+            piTextView.setText(savedInstanceState.getCharSequence(KEY_PI_TEXT));
+            cppTimeTextView.setText(savedInstanceState.getCharSequence(KEY_CPP_TIME_TEXT));
+            javaTimeTextView.setText(savedInstanceState.getCharSequence(KEY_JAVA_TIME_TEXT));
+        }
     }
 
     public void calculatePI(View view) {
-        EditText editText = (EditText) findViewById(R.id.stepsEditText);
-        TextView piTextView = (TextView) findViewById(R.id.PiTextView);
-        TextView timeTextView = (TextView) findViewById(R.id.timeTextView);
-        TextView time2TextView = (TextView) findViewById(R.id.time2TextView);
+        editText = (EditText) findViewById(R.id.stepsEditText);
+        piTextView = (TextView) findViewById(R.id.PiTextView);
+        //chronoTimeTextView = (TextView) findViewById(R.id.ChronoTimeTextView);
+        cppTimeTextView = (TextView) findViewById(R.id.CppTimeTextView);
+        javaTimeTextView = (TextView) findViewById(R.id.JavaTimeTextView);
         String stepsString = editText.getText().toString();
-        CheckBox javaCheckBox = (CheckBox) findViewById(R.id.checkBox);
+        javaCheckBox = (CheckBox) findViewById(R.id.javaCheckBox);
+        cppCheckBox = (CheckBox) findViewById(R.id.cppCheckBox);
 
 
         long steps = Long.parseLong(stepsString);
-        long t1Start = System.currentTimeMillis();
-        double pi1 = piFromJNI(steps);
-        long t1End = System.currentTimeMillis();
-        long t1Delta = t1End - t1Start;
-        double elapsed1Seconds = t1Delta / 1000.0;
-        piTextView.setText(String.valueOf(pi1));
-        timeTextView.setText(String.valueOf(timeFromJNI(steps)));
-        time2TextView.setText(String.valueOf(elapsed1Seconds));
+        
+        if (cppCheckBox.isChecked()) {
+            long t1Start = System.currentTimeMillis();
+            double pi1 = piFromJNI(steps, threadsNum);
+            long t1End = System.currentTimeMillis();
+            long t1Delta = t1End - t1Start;
+            double elapsed1Seconds = t1Delta / 1000.0;
+            piTextView.setText(String.valueOf(pi1));
+            //chronoTimeTextView.setText(String.valueOf(timeFromJNI(steps)));
+            cppTimeTextView.setText(String.valueOf(elapsed1Seconds));
+        }else{
+            cppTimeTextView.setText("N/A");
+        }
         //((Activity) context).runOnUiThread(new Runnable() {
         //    @Override
         //    public void run() {
@@ -57,17 +120,16 @@ public class MainActivity extends AppCompatActivity {
         //piTextView.postInvalidate();
         //timeTextView.postInvalidate();
         //time2TextView.postInvalidate();
-        TextView time3TextView = (TextView) findViewById(R.id.time3TextView);
         if (javaCheckBox.isChecked()) {
             long t2Start = System.currentTimeMillis();
             double pi2 = Pi.getPI(steps);
             long t2End = System.currentTimeMillis();
             long t2Delta = t2End - t2Start;
             double elapsed2Seconds = t2Delta / 1000.0;
-            time3TextView.setText(String.valueOf(elapsed2Seconds));
+            javaTimeTextView.setText(String.valueOf(elapsed2Seconds));
             //time3TextView.postInvalidate();
         }else{
-            time3TextView.setText("N/A");
+            javaTimeTextView.setText("N/A");
         }
     }
 
@@ -75,6 +137,6 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native double piFromJNI(long steps);
+    public native double piFromJNI(long steps, int threads);
     public native double timeFromJNI(long steps);
 }
