51034d1e8da5892718f14d718e56544fd98b3ed9
Andrea Ferraro <andrea.ferraro@cnaf.infn.it>
2018-02-01 12:23:52 +0100

    Bugfix

--- app/src/main/java/it/ttlab/piloopapp/Pi.java
+++ app/src/main/java/it/ttlab/piloopapp/Pi.java
@@ -2,12 +2,12 @@ package it.ttlab.piloopapp;
 
 /**
  * Created by Andrea Ferraro on 07/05/2017.
+ * INFN TTLab (https://ttlab.infn.it/)
+ *
  */
 
 class Pi {
-    //private double x, pi, sum, step ;
 
-    // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
@@ -17,9 +17,9 @@ class Pi {
     static double getPI(long num_steps){
         double x, pi, sum =0;
         double step = 1.0d / (double) num_steps;
-        for (int i=1;i<= num_steps; i++){
-            x = (i-0.5)*step;
-            sum = sum + 4.0/(1.0+x*x);
+        for (int i=1; i<= num_steps; i++){
+            x = (i-0.5) * step;
+            sum = sum + 4.0 / (1.0+x*x);
         }
         pi = step * sum;
         return pi;
